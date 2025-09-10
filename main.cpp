7a2e8d876dd7ec5a60b3d6d161da0ea6cfdeb7a4
itsmecharliev <45504444+itsmecharliev@users.noreply.github.com>
2025-09-10 11:44:59 -0700

    Resolved merge conflict
    Added displaying of operation

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << x << "+" << y << "="<< x + y << std::endl;
+    std::cout << x << "-" << y << "="<< x - y << std::endl;
+    std::cout << x << "*" << y << "="<< x * y << std::endl;
+    std::cout << x << "/" << y << "="<< x / y << " with a remainder of " << x%y << std::endl;
+    std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << "=" << pow(x, y) << std::endl;
 
     return 0;
 }
